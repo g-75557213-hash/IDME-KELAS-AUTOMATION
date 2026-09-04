bd761b3e26ada642754c37de37e2aedf2d5e1c1e
ChairelAshman <g-75557213@moe-dl.edu.my>
2026-09-04 18:36:06 +0800

    build: configure github pages deployment
    Add github actions workflow and set vite base path to support hosting on github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/IDME-KELAS-AUTOMATION/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
